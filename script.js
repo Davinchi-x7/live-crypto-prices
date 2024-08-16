2a7d1c405029703e2e52483a16e7a4fa4e572a06
Davinchi-x7 <dufgvjdu@gmail.com>
2024-08-16 23:19:51 +0300

    minor update

--- script.js
+++ script.js
@@ -28,29 +28,3 @@ $.ajax(liveprice).done(function (response){
 
 
 
-/*function updatePrices() {
-    $.ajax({
-        url: "https://api.coingecko.com/api/v3/simple/price?&ids=bitcoin%2Cethereum%2Cworldcoin%2Cdogecoin&vs_currencies=usd", // Replace 'liveprice' with the actual URL string if it's not a variable
-        method: 'GET', // Assuming you're using a GET request
-        success: function(response) {
-            // Update the DOM elements with the latest prices
-            btc.innerHTML = response.bitcoin.usd;
-            eth.innerHTML = response.ethereum.usd;
-            wld.innerHTML = response.worldcoin.usd;
-            doge.innerHTML = response.dogecoin.usd;
-            
-            // Log the response for debugging
-            console.log(response);
-        },
-        error: function(error) {
-            console.error('Error fetching live prices:', error);
-        }
-    });
-}
-
-// Call the function immediately to get the prices on page load
-updatePrices();
-
-// Set interval to update prices every 5 seconds (5000 milliseconds)
-setInterval(updatePrices, 5000);
-*/
